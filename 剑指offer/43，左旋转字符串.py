54cbf81b84b79ad3fb914225330ec027ca41d877
jiangxinyang <334973834@qq.com>
2019-12-23 19:15:40 +0800

    add

--- 剑指offer/43，左旋转字符串.py
+++ 剑指offer/43，左旋转字符串.py
@@ -3,3 +3,15 @@
 对于一个给定的字符序列S，请你把其循环左移K位后的序列输出。例如，字符序列S=”abcXYZdef”,要求输出循环左移3位后的结果，
 即“XYZdefabc”。是不是很简单？OK，搞定它！
 """
+
+
+def left_rotate_string(s, n):
+    if not s:
+        return ""
+    n %= len(s)
+    return s[n:] + s[:n]
+
+
+string = "abcXYZdef"
+res = left_rotate_string(string, 3)
+print(res)
