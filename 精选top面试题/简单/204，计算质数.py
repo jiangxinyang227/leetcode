7eb26900a3fe214567fb122f712aa70b5b1cbbed
jiangxinyang227 <34847861+jiangxinyang227@users.noreply.github.com>
2020-01-04 20:44:33 +0800

    add

--- 精选top面试题/简单/204，计算质数.py
+++ 精选top面试题/简单/204，计算质数.py
@@ -7,8 +7,26 @@
 输出: 4
 解释: 小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
 """
+from math import sqrt
 
 
 class Solution:
     def countPrimes(self, n):
-        pass
+        if n < 2:
+            return 0
+        count = 0
+        for i in range(2, n):
+            if self.isPrime(i):
+                count += 1
+        return count
+
+    def isPrime(self, n):
+        for i in range(2, int(sqrt(n)) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+
+s = Solution()
+res = s.countPrimes(7)
+print(res)
