925c309c423670aefe23e33339fd423f64f331c6
jiangxinyang <334973834@qq.com>
2020-01-17 01:48:22 +0800

    add

---
+++ 排序/1，插入排序.py
@@ -0,0 +1,16 @@
+"""
+从第一个元素开始，和第0个元素对比，先排序好这两个元素，然后依次拿后面的元素和前面的元素比较
+第i个元素和前i个元素比较，通过从和i-1 一直到0，和各个元素比较交换，当不满足条件时就停止交换
+"""
+
+
+def insert_sort(nums):
+    for i in range(1, len(nums)):
+        while nums[i] < nums[i - 1] and i > 0:
+            nums[i], nums[i - 1] = nums[i - 1], nums[i]
+            i -= 1
+
+
+array = [3, 2, 4, 5, 1, 6]
+insert_sort(array)
+print(array)
---
+++ 排序/4，冒泡排序.py
@@ -0,0 +1,16 @@
+"""
+把最大的元素往后放
+"""
+
+
+def bubble_sort(nums):
+    for i in range(len(nums) - 1, 0, -1):
+        for j in range(i):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    return nums
+
+
+array = [3, 2, 4, 5, 1, 6]
+bubble_sort(array)
+print(array)
